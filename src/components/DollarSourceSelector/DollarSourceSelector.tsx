afe24fcd5e72290cf3cdfb9d8b5822e07e8ebc97
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-01-13 01:05:33 -0400

    WIP(dollar source selector): add styles to listbox options

--- src/components/DollarSourceSelector/DollarSourceSelector.tsx
+++ src/components/DollarSourceSelector/DollarSourceSelector.tsx
@@ -1,44 +1,40 @@
-import { Listbox } from "@headlessui/react";
-import { useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 import type { SelectOption } from "../../types/interfaces";
 
-const options: SelectOption[] = [
-  {
-    value: 1,
-    text: "BCV",
-  },
-  {
-    value: 2,
-    text: "EnParaleloVzla"
-  },
-  {
-    value: 3,
-    text: "DolarToday"
-  },
-  {
-    value: 4,
-    text: "MonitorDolar"
-  },
-  {
-    value: 5,
-    text: "Binance"
-  }
-]
+interface DollarSourceSelectorProps {
+  options: SelectOption[]
+}
 
-const DollarSourceSelector = () => {
+const DollarSourceSelector = ({ options }: DollarSourceSelectorProps) => {
   const [source, setSource] = useState<SelectOption>(options[0])
 
   return(
-    <div className="relative">
+    <div className="relative w-52 mx-auto">
     <Listbox value={source} onChange={setSource}>
-      <Listbox.Button className="w-52 py-1 bg-white text-slate-800 rounded-sm shadow-sm">{source.text}</Listbox.Button>
-      <Listbox.Options className="absolute mt-1 w-full">
-        {options.map(option => (
-          <Listbox.Option key={option.value} value={option}>
-            {option.text}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+      <Listbox.Button className="py-1 w-full bg-white text-slate-800 rounded-md shadow-sm">{source.text}</Listbox.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-in duration-100"
+        enterFrom="opacity-30"
+        enterTo="opacity-100"
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <Listbox.Options className="absolute mt-2 w-full text-slate-800 rounded-md shadow-sm overflow-hidden">
+          {options.map(option => (
+            <Listbox.Option key={option.value} value={option}>
+              {({ active, selected }) => (
+                <li className={`${selected ? "bg-slate-600 text-white" : "bg-white"} py-1`}>
+                  {option.text}
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Transition>
     </Listbox>
     </div>
 
